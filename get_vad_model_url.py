976facc813ea5e23f017d4b655f4d7ee227904e2
Vincenzo <vincenzo.timmel@fhnw.ch>
2024-12-02 11:22:47 +0100

    Adds method to download the models, which then have to be copied.

--- get_vad_model_url.py
+++ get_vad_model_url.py
@@ -6,4 +6,4 @@ import whisperx
 
 import os
 os.environ['HF_HOME'] = 'hf_home'
-model = whisperx.load_model('/home/kenfus/.cache/huggingface/hub/models--Systran--faster-whisper-large-v3/snapshots/edaa852ec7e145841d8ffdb056a99866b5f0a478', device='cuda')
+model = whisperx.load_model('large-v3', device='cuda')
